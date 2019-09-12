34043030eb57c61663ccdd96d7ebbe43619c05fb
maryellendecaro <maryellendecaro@10.0.0.67>
2019-09-12 12:26:53 -0400

    ready

---
+++ HW1/src/HW1.java
@@ -0,0 +1,18 @@
+
+public class HW1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("All of the birds died ");
+		System.out.println("in 1986 ");
+		System.out.println("due to Reagan ");
+		System.out.println("killing them ");
+		System.out.println("with spies ");
+		System.out.println("who are now ");
+		System.out.println("watching us. ");
+		System.out.println("The birds work ");
+		System.out.println("for the ");
+		System.out.println("Bourgeoisie.");
+	}
+
+}
